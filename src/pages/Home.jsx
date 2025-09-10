02aef2b43550a834365a21c6088d625f7445dfbc
getdeveopsready <getdevopsread@gmai.com>
2025-09-10 17:20:08 +0530

    Resolving Error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Video from "../components/home/Video";
-import HomeHeroText from "../components/home/HomeHeroText";
-import HomeBottomText from "../components/home/HomeBottomText";
+import Video from "../components/Home/video.jsx";
+import HomeHeroText from "../components/Home/HomeHeroText.jsx";
+import HomeBottomText from "../components/Home/HomeBottomText.jsx";
 
 const Home = () => {
   return (
@@ -21,5 +21,6 @@ const Home = () => {
 };
 
 export default Home;
+
 
 
